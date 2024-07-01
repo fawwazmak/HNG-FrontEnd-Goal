c0cfcb566bd7a5e4d03388db6b93e0852c893bc2
fawwazmak <fawwazmakinde47@gmail.com>
2024-07-01 12:46:52 +0100

    added js

---
+++ index.js
@@ -0,0 +1,30 @@
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+setInterval(() => {
+    const time = new Date();
+    const dayNum = time.getUTCDate();
+    const currentDay = daysOfWeek[dayNum];
+
+    const currentHour = time.getUTCHours();
+    const currentMinute = time.getUTCMinutes();
+    const currentSecond = time.getUTCSeconds();
+    const currentTime = `${currentHour} : ${currentMinute} : ${currentSecond}`;
+
+    // alert(currentHour);
+    crrntTime = document.getElementById("currentTime");
+    crrntTime.innerText = currentTime;
+
+    crrntDay = document.getElementById("crrntDay");
+
+    crrntDay.innerText = currentDay;
+}, 1000)
+
+
